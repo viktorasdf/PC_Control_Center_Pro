1804bf90db3b1eb96a6c6826ccc3595a34d9e186
vik <vik@example.com>
2026-08-02 10:02:52 +0300

    Clean repository structure

--- app/pages/network.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
-
-
-class NetworkPage(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Network"))
--- app/pages/storage.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
-
-
-class StoragePage(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Storage"))
